00aff0d4007372c607dee70ed1eae91c99728d03
chat2004 <huynhminhvu@gmail.com>
2018-06-13 16:20:12 +0700

    update code

--- first_react_app/src/App.js
+++ first_react_app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import classes from './App.css';
 // import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
@@ -43,16 +43,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -67,25 +59,22 @@ class App extends Component {
           })}
         </div>
       );
-
-      style.backgroundColor = 'red';
-      
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi! I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button 
-            style={style}
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass}
             onClick={this.togglePersonHandler}>Toggle Persons
           </button>
           {/* <button onClick={() => this.switchNameHandler('Minh Vu')}>Swith Name</button> */}
