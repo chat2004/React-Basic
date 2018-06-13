9a953868d80a87c0eea0537527318965766fcd3f
chat2004 <huynhminhvu@gmail.com>
2018-06-13 15:32:15 +0700

    add new code, before eject

--- first_react_app/src/Person/Person.js
+++ first_react_app/src/Person/Person.js
@@ -1,7 +1,20 @@
 import React from 'react';
+// import Radium from 'radium';
+import './Person.css'
 
-const person = () => {
-    return <p>I'm a Person!</p>
+const person = (props) => {
+    // const style = {
+    //     '@media (min-width: 500px)': {
+    //         width: '450px'
+    //     }
+    // };
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name}! And I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
 }
 
 export default person;
