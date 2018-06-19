922e4a4f9812a69474428c3a4e460853f7819355
chat2004 <huynhminhvu@gmail.com>
2018-06-19 08:57:16 +0700

    finish burger project

--- burger_builder_after_navigation/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger_builder_after_navigation/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Aux from '../../../hoc/Auxiliary';
 import modal from '../../UI/Modal/Modal';
 
 const sideDrawer = (props) => {
