d1a04308c3c357a39a1513429b25b3cf059e5d21
chat2004 <huynhminhvu@gmail.com>
2018-06-21 09:20:28 +0700

    add routing

--- blog_routing/src/containers/Blog/Blog.js
+++ blog_routing/src/containers/Blog/Blog.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import axios from '../../axios';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
-
+import asyncComponent from '../../hoc/asyncComponent';
 import './Blog.css';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 
 class Blog extends Component {
+    state = {
+        auth: true
+    }
 
     render () {
         return (
@@ -30,8 +37,12 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {/* {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null} */}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>} />>
+                    {/* <Redirect from="/" to="/posts" />> */}
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
                 
                 
