9a1e7506865595b092eda4cae14da73eb284613f
chat2004 <huynhminhvu@gmail.com>
2018-07-04 21:34:46 +0700

    add webpack and nextjs

---
+++ webpack_basic/src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The pizza</h1>
+                <p>Awesome users onboard in this course</p>
+            </div>
+        );
+    }
+}
+
+export default Users;   
